992fdf3f017fe81ff743fdf0a914b5bba0fbbc15
aprioul <arnaud.prioul@genious-interactive.com>
2015-07-01 17:22:59 +0200

    add message

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -4,9 +4,20 @@
 (function ($, w) {
 
     $(document).ready(function() {
-        $(".text-field--group__input").origamInput();
-        $(".text-field--group__input[type=password]").origamPassword();
-        $(".text-field--group__input[type=phone]").origamPhone();
+        if($('#form').length > 0) {
+            if ($(".text-field--group__input").length > 0) {
+                $(".text-field--group__input").origamInput();
+            }
+            if ($(".text-field--group__input[type=password]").length > 0) {
+                $(".text-field--group__input[type=password]").origamPassword();
+            }
+            if ($(".text-field--group__input[type=phone]").length > 0) {
+                $(".text-field--group__input[type=phone]").origamPhone();
+            }
+        }
+        if($("#message-notification").length > 0) {
+
+        }
     });
 
 })(jQuery, window);
