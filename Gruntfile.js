4aca71c86a2bd16bc7e257b84e858e1f586ace1c
aprioul <arnaud.prioul@genious-interactive.com>
2015-07-03 11:21:22 +0200

    Grunt project cleaning

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,6 +22,45 @@ module.exports = function(grunt) {
             }
         },
 
+        copy: {
+            main: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'assets/fonts/',
+                        src: '**',
+                        dest: 'dist/fonts/',
+                        flatten: true,
+                        filter: 'isFile'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'dist/js/cpr/',
+                        src: ['**/*.js'],
+                        dest: 'dist/js/',
+                        rename: function(dest, src) {
+                            return dest+src.replace('.js', '.min.js');
+                        }
+                    }
+                ]
+            }
+        },
+
+        uglify: {
+            build: {
+                files: [{
+                    expand: true,
+                    src: '**/*.js',
+                    dest: 'dist/js/cpr',
+                    cwd: 'dist/js/src/'
+                }]
+            }
+        },
+
+        clean: [
+            "dist/js/cpr"
+        ],
+
         watch: {
             options: {
                 livereload: true // Activons le livereload du navigateur
@@ -51,7 +90,7 @@ module.exports = function(grunt) {
                 // and combine into a single js file per module
                 concat[dirName] = {
                     src: [dir + '/*.js'],
-                    dest: 'dist/js/' + dirName + '.min.js'
+                    dest: 'dist/js/src/' + dirName + '.js'
                 };
 
                 // add module subtasks to the concat task in initConfig
@@ -64,7 +103,10 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-csso');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 
     // J'assigne ma tâche à la commande par défaut de Grunt
-    grunt.registerTask("default", ["csso", "prepareModules", "concat"]);
+    grunt.registerTask("default", ["csso", "prepareModules", "concat", "uglify", "copy", "clean"]);
 };
