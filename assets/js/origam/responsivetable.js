f039100f90ae148ef5e4d37a34fa74be6d0abc1c
aprioul <arnaud.prioul@genious-interactive.com>
2015-07-21 17:41:20 +0200

    Init table responsive

--- assets/js/origam/responsivetable.js
+++ assets/js/origam/responsivetable.js
@@ -26,16 +26,46 @@
     Table.TRANSITION_DURATION = 1000;
 
     Table.DEFAULTS = {
-
+        addRowToggle: true,
+        toggleSelector: ' > tbody > tr:not(.footable-row-detail)', //the selector to show/hide the detail row
+        columnDataSelector: '> thead > tr:last-child > th, > thead > tr:last-child > td', //the selector used to find the column data in the thead
+        detailSeparator: ':', //the separator character used when building up the detail row
+        toggleHTMLElement: '<div />', // override this if you want to insert a click target rather than use a background image.
+        classes: {
+            main: 'responsivetable',
+            loading: 'responsivetable-loading',
+            loaded: 'responsivetable-loaded',
+            toggle: 'responsivetable-toggle',
+            disabled: 'responsivetable-disabled',
+            detail: 'responsivetable-row-detail',
+            detailCell: 'responsivetable-row-detail-cell',
+            detailInner: 'responsivetable-row-detail-inner',
+            detailInnerRow: 'responsivetable-row-detail-row',
+            detailInnerGroup: 'responsivetable-row-detail-group',
+            detailInnerName: 'responsivetable-row-detail-name',
+            detailInnerValue: 'responsivetable-row-detail-value',
+            detailShow: 'responsivetable-detail-show'
+        }
     };
 
     Table.prototype.init = function (type, element, options) {
-        this.type      = type;
-        this.element   = element;
-        this.$element  = $(element);
-        this.options   = this.getOptions(options);
+        this.type       = type;
+        this.element    = element;
+        this.$element   = $(element);
+        this.options    = this.getOptions(options);
 
+        var column = [];
 
+        this.$element.find('thead th').each(function (index, e) {
+            var $column = $(e);
+
+            var priority = parseInt($column.data('priority'), 10);
+
+            var priorityWidth = $column.outerWidth();
+            $column.css('width', priorityWidth);
+
+            (typeof(column[priority]) != 'undefined') ? column[priority]['width'] += priorityWidth : column[priority] = {width: priorityWidth};
+        });
 
     };
 
@@ -47,6 +77,26 @@
         options = $.extend({}, this.getDefaults(), this.$element.data(), options);
 
         return options
+    };
+
+    Table.prototype.addRowToggle = function () {
+
+    };
+
+    Table.prototype.toggleDetail = function () {
+
+    };
+
+    Table.prototype.setDetailRow = function () {
+
+    };
+
+    Table.prototype.tableResize = function () {
+
+    };
+
+    Table.prototype.CalculateWidth = function () {
+
     };
 
     // TABLE PLUGIN DEFINITION
@@ -79,7 +129,7 @@
     };
 
     $(document).ready(function() {
-        $('[data-form="table"]').table();
+        $('[data-app="table"]').table();
     });
 
 })(jQuery, window);
