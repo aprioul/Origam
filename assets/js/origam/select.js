fca1211c03cd803be66f85f0941849160315d3b2
aprioul <arnaud.prioul@genious-interactive.com>
2015-08-06 17:32:35 +0200

    Start select.js

--- assets/js/origam/select.js
+++ assets/js/origam/select.js
@@ -1,9 +1,8 @@
+
 /**
  * Apply origamSelect
+ *
  */
-
-// SELECT PLUGIN DEFINITION
-// =========================
 
 (function ($, w) {
 
@@ -13,35 +12,53 @@
     // ===============================
 
     var Select = function (element, options) {
-        this.type = null;
-        this.options = null;
-        this.$element = null;
+        this.type       = null;
+        this.options    = null;
+        this.focus    = null;
+        this.blur    = null;
+        this.$element   = null;
 
         this.init('select', element, options)
     };
 
-    if (!$.fn.input) throw new Error('Select requires input.js');
-
-    Select.VERSION = '0.1.0';
+    Select.VERSION  = '0.1.0';
 
     Select.TRANSITION_DURATION = 1000;
 
-    Select.DEFAULTS = $.extend({}, $.fn.input.Constructor.DEFAULTS, {});
-
-    Select.prototype = $.extend({}, $.fn.input.Constructor.prototype);
-
-    Select.prototype.constructor = Select;
-
-    Select.prototype.event = function (options) {
-
+    Select.DEFAULTS = {
+        
     };
+
+    Select.prototype.init = function (type, element, options) {
+        this.type      = type;
+        this.element   = element;
+        this.$element  = $(element);
+        this.options   = this.getOptions(options);
+        this.$parent   = '.' + this.options.parentNode;
+        
+        
+    };
+
+    Select.prototype.getDefaults = function () {
+        return Select.DEFAULTS
+    };
+
+    Select.prototype.getOptions = function (options) {
+        options = $.extend({}, this.getDefaults(), this.$element.data(), options);
+
+        return options
+    };
+
+    // SELECT PLUGIN DEFINITION
+    // =========================
 
     function Plugin(option) {
         return this.each(function () {
-            var $this = $(this);
-            var data = $this.data('origam.select');
+            var $this   = $(this);
+            var data    = $this.data('origam.select');
             var options = typeof option == 'object' && option;
 
+            if (!data && /destroy|hide/.test(option)) return;
             if (!data) $this.data('origam.select', (data = new Select(this, options)));
             if (typeof option == 'string') data[option]()
         })
@@ -49,19 +66,19 @@
 
     var old = $.fn.select;
 
-    $.fn.select = Plugin;
+    $.fn.select             = Plugin;
     $.fn.select.Constructor = Select;
 
 
     // SELECT NO CONFLICT
     // ===================
 
-    $.fn.input.noConflict = function () {
+    $.fn.select.noConflict = function () {
         $.fn.select = old;
         return this
     };
 
-    $(document).ready(function () {
+    $(document).ready(function() {
         $('[data-form="select"]').select();
     });
 
