ebfedd0bd6579a4070f12f4a34bb55fa69f316d4
aprioul <arnaud.prioul@genious-interactive.com>
2016-02-25 22:49:26 +0100

    fix

--- v2/Modules/Grid/index.php
+++ v2/Modules/Grid/index.php
@@ -1,19 +1,19 @@
-<?php include "../../Include/helpers.inc"; ?>
-<?php include "../../Include/variables.inc"; ?>
+<?php include $base_path . "/Include/helpers.inc"; ?>
+<?php include $base_path . "/Include/variables.inc"; ?>
 <?php include "Include/variables.inc"; ?>
 <!DOCTYPE html>
 <html>
 <head lang="en">
-  <?php include "../../Templates/head.tpl.php"; ?>
+  <?php include $base_path . "/Templates/head.tpl.php"; ?>
 </head>
 <body>
   <div class="origam-grid">
     <div class="origam-grid__wrapper">
       <div class="origam-grid__section">
-        <?php include "../../Templates/title.tpl.php"; ?>
+        <?php include $base_path . "/Templates/title.tpl.php"; ?>
         <?php foreach($sections as $section): ?>
           <?php include "Include/" . $section . ".inc"; ?>
-          <?php include "../../Templates/section.tpl.php"; ?>
+          <?php include $base_path . "/Templates/section.tpl.php"; ?>
         <?php endforeach; ?>
       </div>
     </div>
